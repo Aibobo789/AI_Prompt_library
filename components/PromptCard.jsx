f995c14cae960ccb88aaf0853a71ef01c34a125a
Aibobo789 <786822142@qq.com>
2024-08-11 18:14:48 +1000

    edit and delete prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -14,6 +14,9 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     navigator.clipboard.writeText(post.prompt);
     setTimeout(()=>setCopied(""),3000);
   };
+  const {data:session} = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   return (
     <div className="prompt_card">
@@ -56,6 +59,24 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id &&
+      pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )
+      } 
     </div>
   )
 }
